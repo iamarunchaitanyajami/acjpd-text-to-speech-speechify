86d997838fd0c25c0bba13061fd4a76f68a70092
iamarunchaitanyajami <iamarunchaitanyajami@gmail.com>
2025-03-12 20:30:16 +0000

    Update from https://github.com/arunchaitanyajami/acjpd-speechify-text-to-speech/commit/218d2f41b28a1d31ee18e3f9ffbc5b24583669e2

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '46babda0a326696e2b89');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '683a3f65ed09186f63ed');
